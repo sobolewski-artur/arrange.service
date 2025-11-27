f29b71dbc10c6dd646ea365889530f726b4cc952
Artur Sobolewski <sobolewski.artur88@gmail.com>
2025-11-27 19:15:23 +0100

    Commit to push on github

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,12 +1,14 @@
 <script setup>
-const { t } = useI18n()
 const localePath = useLocalePath()
 const { locales } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
 </script>
 
 <template>
-    <NuxtLink class="mx-3" :to="localePath('/')">Home</NuxtLink>
-            <NuxtLink class="mx-3" :to="localePath('/about')">About</NuxtLink>
-    <NuxtLink class="mx-3" v-for="{code, name} in locales" :key="code" :to="switchLocalePath(code)">{{ code.toUpperCase() }}</NuxtLink>
+    <div>
+        <NuxtLink class="mx-3" :to="localePath('/')">Home</NuxtLink>
+        <NuxtLink class="mx-3" :to="localePath('/about')">About</NuxtLink>
+        <NuxtLink class="mx-3" v-for="{code, name} in locales" :key="code" :to="switchLocalePath(code)">{{ name }}</NuxtLink>
+        <NuxtLink class="mx-3" :to="localePath('/login')">Log in</NuxtLink>
+    </div>
 </template>
---
+++ app/pages/login.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello from login page</h1>
+</template>
