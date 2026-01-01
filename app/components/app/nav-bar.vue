b334e2fb9a35c655e1f6fb2ce25dd44806b69be5
Artur Sobolewski <sobolewski.artur88@gmail.com>
2026-01-01 16:09:19 +0100

    Success with refactor navlinks

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -3,75 +3,54 @@ import { getAuth, signOut } from 'firebase/auth'
 const localePath = useLocalePath()
 const { t } = useI18n()
 const route = useRoute()
+const router = useRouter()
 const user = useState('user')
 const showLogout = ref(false)
 const showPost = ref(false)
+
+const home = computed(() => route.fullPath.split('/')[1] === 'home')
+const search = computed(() => route.fullPath.split('/')[1] === 'search')
+const notifications = computed(() => route.fullPath.split('/')[1] === 'notifications')
+const messages = computed(() => route.fullPath.split('/')[1] === 'messages')
+const settings = computed(() => route.fullPath.split('/')[1] === 'settings')
+const profile = computed(() => route.fullPath.split('/')[1] === 'profile')
+const post = computed(() => route.fullPath.split('/')[1] === 'post')
+const signin = computed(() => route.fullPath.split('/')[1] === 'signin')
+const signout = computed(() => route.fullPath.split('/')[1] === 'signout')
 
 
 function logOut() {
     signOut(getAuth())
     showLogout.value = false
 }
+function onShowLogout() {
+    showLogout.value = false
+    router.go(-1)
+}
 </script>
 
 <template>
-    <nav class="flex flex-col justify-between sticky top-0 bottom-0 border-r-2 h-screen">
-        <div>
-            <NuxtLink class="nav-bar-link" :to="localePath('/home')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'home'" name="icon-park-outline:home"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'home'" name="icon-park-solid:home"></icon>
-                <span class="btn-name">{{ t('home-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink class="nav-bar-link" :to="localePath('/search')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'search'" name="icon-park-outline:search"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'search'" name="icon-park-solid:search"></icon>
-                <span class="btn-name" >{{ t('search-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink class="nav-bar-link" :to="localePath('/notifications')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'notifications'" name="line-md:bell"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'notifications'" name="line-md:bell-filled"></icon>
-                <span class="btn-name" >{{ t('notifications-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink class="nav-bar-link" :to="localePath('/messages')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'messages'" name="bx:envelope"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'messages'" name="bxs:envelope"></icon>
-                <span class="btn-name" >{{ t('messages-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink class="nav-bar-link" :to="localePath('/settings')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'settings'" name="ic:outline-settings"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'settings'" name="ic:baseline-settings"></icon>
-                <span class="btn-name" >{{ t('settings-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink class="nav-bar-link" :to="localePath('/profile')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'profile'" name="iconamoon:profile-light"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'profile'" name="iconamoon:profile-fill"></icon>
-                <span class="btn-name" >{{ t('profile-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink class="nav-bar-link" :to="localePath('/post')">
-                <icon v-if="!showPost" name="icon-park-outline:write"></icon>
-                <icon v-if="showPost" name="icon-park-solid:write"></icon>
-                <span class="btn-name">{{ t('post-btn') }}</span>
-            </NuxtLink>
+    <nav class="flex flex-col justify-between sticky top-0 border-r-2 h-screen">
+        <div class="flex flex-col">
+            <AppNavButton :to="localePath('/home')" :showSolid="home" icon1="icon-park-outline:home" icon2="icon-park-solid:home">{{ t('home-btn') }}</AppNavButton>
+            <AppNavButton :to="localePath('/search')" :showSolid="search" icon1="icon-park-outline:search" icon2="icon-park-solid:search">{{ t('search-btn') }}</AppNavButton>
+            <AppNavButton :to="localePath('/notifications')" :showSolid="notifications" icon1="line-md:bell" icon2="line-md:bell-filled">{{ t('notifications-btn') }}</AppNavButton>
+            <AppNavButton :to="localePath('/messages')" :showSolid="messages" icon1="bx:envelope" icon2="bxs:envelope">{{ t('messages-btn') }}</AppNavButton>
+            <AppNavButton :to="localePath('/settings')" :showSolid="settings" icon1="ic:outline-settings" icon2="ic:baseline-settings">{{ t('settings-btn') }}</AppNavButton>
+            <AppNavButton :to="localePath('/profile')" :showSolid="profile" icon1="iconamoon:profile-light" icon2="iconamoon:profile-fill">{{ t('profile-btn') }}</AppNavButton>
+            <AppNavButton :to="localePath('/post')" :showSolid="post" icon1="icon-park-outline:write" icon2="icon-park-solid:write">{{ t('post-btn') }}</AppNavButton>
         </div>
         <div>
-            <NuxtLink v-if="!user" class="nav-bar-link" :to="localePath('/signin')">
-                <icon v-if="route.fullPath.split('/')[1] !== 'signin'" name="clarity:sign-in-line"></icon>
-                <icon v-if="route.fullPath.split('/')[1] === 'signin'" name="clarity:sign-in-solid"></icon>
-                <span class="btn-name">{{ t('signin-btn') }}</span>
-            </NuxtLink>
-            <NuxtLink v-if="user" class="nav-bar-link" @click="showLogout = true">
-                <icon v-if="!showLogout" name="clarity:sign-out-line"></icon>
-                <icon v-if="showLogout" name="clarity:sign-out-solid"></icon>
-                <span class="btn-name">{{ t('signout-btn') }}</span>
-            </NuxtLink>
+            <AppNavButton v-if="!user" :to="localePath('/signin')" :showSolid="signin" icon1="clarity:sign-in-line" icon2="clarity:sign-in-solid">{{ t('signin-btn') }}</AppNavButton>
+            <AppNavButton v-if="user" @click="showLogout = true" :to="localePath('/signout')" :showSolid="showLogout" icon1="clarity:sign-out-line" icon2="clarity:sign-out-solid">{{ t('signout-btn') }}</AppNavButton>
         </div>
         <Teleport to="body">
             <Transition name="modal">
-                <LayoutModalBg v-if="showLogout">
+                <LayoutModalBg v-if="showLogout" @click.self="onShowLogout">
                     <LayoutFrame>
                         <h2>{{ t('signout.modal-title') }}</h2>
                         <div class="mt-6 flex gap-10">
-                            <LayoutButton @click="showLogout = false">{{ t('signout.btn-abort') }}</LayoutButton>
+                            <LayoutButton @click="onShowLogout">{{ t('signout.btn-abort') }}</LayoutButton>
                             <NuxtLink @click="logOut" :to="localePath('/signin')"><LayoutButton>{{ t('signout.btn-confirm') }}</LayoutButton></NuxtLink>
                         </div>
                     </LayoutFrame>
---
+++ app/components/app/nav-button.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtLink class="flex items-center hover:bg-slate-800 rounded-lg px-4 m-1 py-2 gap-2 text-3xl">
+        <icon v-if="!showSolid" :name="icon1"></icon>
+        <icon v-if="showSolid" :name="icon2"></icon>
+        <span class="hidden lg:flex"><slot /></span>
+    </NuxtLink>
+</template>
+
+<script setup>
+defineProps(['icon1', 'icon2', 'showSolid'])
+</script>
